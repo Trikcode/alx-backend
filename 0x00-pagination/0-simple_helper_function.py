38913ae023bdcaff2c8545c8b9c4282e9ab34113
Trikcode <ayesiganobert@gmail.com>
2022-04-29 22:19:29 +0300

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 """
-This is a simple helper function to find
-start and end pagination parameters.
+Contains definition of index_range helper function
 """
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Return the start and end index of the range
-    of items to be displayed on the page.
+    Takes 2 integer arguments and returns a tuple of size two
+    containing the start and end index corresponding to the range of
+    indexes to return in a list for those pagination parameters
     Args:
-        page (int): The page number.
-        page_size (int): The number of items per page.
-    Returns:
-        tuple: The start and end index of the range
-        of items to be displayed on the page.
+        page (int): page number to return (pages are 1-indexed)
+        page_size (int): number of items per page
+    Return:
+        tuple(start_index, end_index)
     """
-    end = page * page_size
-    start = end - page_size
-    return start, end
+    start, end = 0, 0
+    for i in range(page):
+        start = end
+        end += page_size
+
+    return (start, end)
