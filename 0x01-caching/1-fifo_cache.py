f60594bd0558d550a6ff0c88809de3abd086237b
Trikcode <ayesiganobert@gmail.com>
2022-05-03 16:26:55 +0300

    data keys removed

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
-""" BaseCaching module """
+""" BaseCaching module
+"""
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFOCache class """
+    """
+    FIFOCache defines a FIFO caching system
+    """
 
     def __init__(self):
-        """ Initialize the class """
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        self.cache_data = {}
-        self.cache_order = []
+        self.order = []
 
     def put(self, key, item):
-        """ Store a key-value pair """
+        """
+        Cache a key-value pair
+        """
         if key is None or item is None:
             pass
         else:
-            length = len(self.cache_order)
-            if length >= BaseCaching.MAX_ITEMS and key in self.cache_data.keys():
-                del self.cache_data[self.cache_order[0]]
-                del self.cache_order[0]
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
             self.cache_data[key] = item
-            self.cache_order.append(key)
 
     def get(self, key):
-        """ Return value linked to key.
-        If key is None or doesn't exist, return None
+        """
+        Return the value linked to a given key, or None
         """
         if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
