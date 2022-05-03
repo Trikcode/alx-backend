f690f39e38b4fe629bcc2d8c6c249b2db663e3da
Trikcode <ayesiganobert@gmail.com>
2022-05-03 16:39:40 +0300

    number should be higher

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,34 +1,44 @@
 #!/usr/bin/env python3
-""" lru cach module """
-
-
-from collections import deque
-BaseCaching = __import__('base_caching').BaseCaching
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ LRU """
+    """
+    FIFOCache defines a FIFO caching system
+    """
 
     def __init__(self):
-        """ constructor """
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        self.queue = deque()
+        self.usage = []
 
     def put(self, key, item):
-        """ put """
-        if key and item:
-            if key in self.cache_data:
-                self.queue.remove(key)
-            elif len(self.cache_data) >= self.MAX_ITEMS:
-                popped = self.queue.popleft()
-                del self.cache_data[popped]
-                print("DISCARD: " + str(popped))
-            self.queue.append(key)
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length > BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.usage[0]))
+                del self.cache_data[self.usage[0]]
+                del self.usage[0]
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """ get """
-        if key in self.cache_data:
-            self.queue.remove(key)
-            self.queue.append(key)
-            return self.cache_data.get(key, None)
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            return self.cache_data[key]
+        return None
