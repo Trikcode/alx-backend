5254a4e251d1f818a7d7e40e030ebd3df6fa5d53
Trikcode <ayesiganobert@gmail.com>
2022-05-03 16:35:23 +0300

    last recently used

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,45 +1,34 @@
-
 #!/usr/bin/env python3
-""" BaseCaching module
-"""
-from base_caching import BaseCaching
+""" lru cach module """
+
+
+from collections import deque
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    FIFOCache defines a FIFO caching system
-    """
+    """ LRU """
 
     def __init__(self):
-        """
-        Initialize the class with the parent's init method
-        """
+        """ constructor """
         super().__init__()
-        self.usage = []
+        self.queue = deque()
 
     def put(self, key, item):
-        """
-        Cache a key-value pair
-        """
-        if key is None or item is None:
-            pass
-        else:
-            length = len(self.cache_data)
-            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.usage[0]))
-                del self.cache_data[self.usage[0]]
-                del self.usage[0]
-            if key in self.usage:
-                del self.usage[self.usage.index(key)]
-            self.usage.append(key)
+        """ put """
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                popped = self.queue.popleft()
+                del self.cache_data[popped]
+                print("DISCARD: " + str(popped))
+            self.queue.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        Return the value linked to a given key, or None
-        """
-        if key is not None and key in self.cache_data.keys():
-            del self.usage[self.usage.index(key)]
-            self.usage.append(key)
-            return self.cache_data[key]
-        return None
+        """ get """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data.get(key, None)
